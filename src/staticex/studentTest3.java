23423692c44c09b313d595913fe1433407b94702
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-04-04 21:58:50 +0900

    20230404

---
+++ src/staticex/studentTest3.java
@@ -0,0 +1,17 @@
+package staticex;
+
+public class studentTest3 {
+
+    public static void main(String[] args) {
+        Student1 studentLee = new Student1();
+        studentLee.setStudentName("이짱민");
+        System.out.println(Student1.serialNum); //serialNum 변수를 직접 클래스 이름으로 참조
+        System.out.println(studentLee.studentName + "학번 " + studentLee.studentID);
+
+
+        Student1 studentSon = new Student1();
+        studentSon.setStudentName("손짱민");
+        System.out.println(Student1.serialNum); //serialNum 변수를 직접 클래스 이름으로 참조
+        System.out.println(studentSon.studentName + "학번: " + studentSon.studentID);
+    }
+}
